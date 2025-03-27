591a48f59034cb10717f83b2b5ee3197d7b7340c
cmj060 <cknd6600@gmail.com>
2025-03-27 17:44:59 +0900

    03/27 backup

---
+++ hellojava/src/ch01/sec06/Hello01.java
@@ -0,0 +1,7 @@
+package ch01.sec06;
+
+public class Hello01 {
+    public static void main(String[] args) {
+        System.out.println("Hello, SalesForceeee");
+    }
+}
---
+++ hellojava/src/ch01/sec06/variabletest.java
@@ -0,0 +1,11 @@
+package ch01.sec06;
+
+public class variabletest {
+
+	public static void main(String[] args) {
+		String var1 = "A" ;
+		System.out.println("\"var1\"");
+
+	}
+
+}
